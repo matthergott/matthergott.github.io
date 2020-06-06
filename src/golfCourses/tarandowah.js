f90d3f7568271b7024c3fd9d1b333843a2e2a97c
Matt Hergott <matt.c.hergott@gmail.com>
2020-06-06 14:56:39 -0400

    Update Tarandowah Website

--- src/golfCourses/tarandowah.js
+++ src/golfCourses/tarandowah.js
@@ -6,7 +6,7 @@ import iconLinks from "../helperFiles/iconLinks";
 const title = 'Tarandowah Golfers Club';
 const city = 'Springfield, ON';
 const designer = 'Martin Hawtree | 2007';
-const website = 'http://www.puregolf.net';
+const website = 'https://tarandowah.ca/';
 const coords = { lat: 42.897755, lng: -80.931557 };
 
 const description = ReactDOMServer.renderToStaticMarkup(initCourse.description(title, city, designer, website));
