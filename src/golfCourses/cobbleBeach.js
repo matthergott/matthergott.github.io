e26ecd1c962961dac1570b207a1f213db9be9447
Matt Hergott <matt.c.hergott@gmail.com>
2020-06-11 07:12:40 -0400

    Change Cobble Beach open date

--- src/golfCourses/cobbleBeach.js
+++ src/golfCourses/cobbleBeach.js
@@ -5,7 +5,7 @@ import iconLinks from "../helperFiles/iconLinks";
 
 const title = 'Cobble Beach Golf Links';
 const city = 'Kemble, ON';
-const designer = 'Doug Carrick | 2006';
+const designer = 'Doug Carrick | 2007';
 const website = 'https://www.cobblebeach.com/Golf/';
 const coords = { lat: 44.673305, lng: -80.926983 };
 
