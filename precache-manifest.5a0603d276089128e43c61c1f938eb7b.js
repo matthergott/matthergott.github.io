c3365e5629ec7a4e49832c76d3ef171aa8c9902c
Matt Hergott <matt.c.hergott@gmail.com>
2020-07-28 00:39:16 -0400

    Updates

--- precache-manifest.5a0603d276089128e43c61c1f938eb7b.js
+++ precache-manifest.5a0603d276089128e43c61c1f938eb7b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4bae4f9b9d45abb3cebd7e77c384989d",
+    "revision": "50d5c285bfbb8b8d3a23563ac91c48ac",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.99f73272.chunk.css"
   },
   {
-    "revision": "3e1a299725a2f115ef62",
+    "revision": "f3ec4d284598f56aa1e4",
     "url": "/static/css/main.ac9fc066.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.13f3fb9c.chunk.js"
   },
   {
-    "revision": "3e1a299725a2f115ef62",
-    "url": "/static/js/main.f11e2af2.chunk.js"
+    "revision": "f3ec4d284598f56aa1e4",
+    "url": "/static/js/main.b5b9a3f2.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e84c4ca7aeddd29111098afee94864f5.js"
+  "/precache-manifest.5a0603d276089128e43c61c1f938eb7b.js"
 );
 
 self.addEventListener('message', (event) => {
